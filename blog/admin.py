79fda0544254ca6ca7269671e79bcb2cdb85bc1a
Kyle Gibson <kyle.gibson@frozenonline.com>
2011-11-18 15:31:37 -0500

    customized admin interface

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,10 @@
 from blog.models import Post
 from django.contrib import admin
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display    = ('visible', 'title', 'pub_date', 'author')
+    list_filter     = ['pub_date']
+    search_fields   = ['title', 'body_mkd']
+    date_hierarchy  = 'pub_date'
+
+admin.site.register(Post, PostAdmin)
