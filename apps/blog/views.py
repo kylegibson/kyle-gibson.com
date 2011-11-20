283025a3c8d5d2cdeea168437a44ef7b08bd743f
Kyle Gibson <kyle.gibson@frozenonline.com>
2011-11-20 17:07:24 -0500

    render shortcut

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,19 +1,13 @@
-from django.shortcuts import render_to_response, get_object_or_404
-from django.template import RequestContext
+from django.shortcuts import render_to_response, get_object_or_404, render
 
 from blog.models import Post
 
 def index(request):
     latest_posts_list = Post.objects.all().order_by('-pub_date')[:5]
-    return render_to_response('blog/templates/post_index.haml', 
-            {'latest_posts_list' : latest_posts_list},
-            context_instance=RequestContext(request))
+    return render(request, 'blog/templates/post_index.haml', locals())
 
 def details(request, post_id):
     post = get_object_or_404(Post, id=post_id)
-    return render_to_response('blog/templates/post_details.haml',
-            {'post' : post},
-            context_instance=RequestContext(request))
-
+    return render(request, 'blog/templates/post_details.haml', locals())
 
 
