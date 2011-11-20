3423ec7b06f1938c442163d3f45066838a6fa191
Kyle Gibson <kyle.gibson@frozenonline.com>
2011-11-19 22:03:10 -0500

    use requestcontext

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 from blog.models import Post
 
 def index(request):
     latest_posts_list = Post.objects.all().order_by('-pub_date')[:5]
     return render_to_response('blog/templates/post_index.haml', 
-            {'latest_posts_list' : latest_posts_list })
+            {'latest_posts_list' : latest_posts_list },
+            context_instance=RequestContext(request))
 
 
