ce67866edfbd0788ad39722412e2c7d3c8673eee
Kyle Gibson <kyle.gibson@frozenonline.com>
2011-11-24 01:55:21 -0500

    named urls

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -7,7 +7,8 @@ v1_api.register(PostResource())
 
 urlpatterns = patterns('blog.views',
     url(r'^api/', include(v1_api.urls)),
-    url(r'^(?P<post_id>\d+)', 'details'),
-    url(r'^$', 'index'),
+    url(r'^(?P<post_id>\d+)', 'details', name='blog_details'),
+    url(r'^(?P<post_id>\d+)(?P<title_slug>.+)', 'details', name='blog_details'),
+    url(r'^$', 'index', name='blog_index'),
 )
 
